0e23ecbebe336cf6b06b7ec40f3cff8deb2686ff
Nate C-K <ncmadcity@gmail.com>
2017-09-04 12:30:27 -0500

    Import script.

---
+++ njc-git-tools.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+git-head-hash() {
+  git rev-parse HEAD
+}
+
+git-current-branch() {
+  git rev-parse --abbrev-ref HEAD
+}
+
+git-unpushed() {
+  git log --oneline $(git-current-branch)..HEAD
+}
+
+git-unpulled() {
+  git log --online HEAD..$(git-current-branch)
+}
+
+git-pull() {
+  CURRENT_HEAD=$(git-head-hash)
+  git pull --rebase
+}
+
+alias g='git status'
+alias ga='git add'
+alias gau='git add -u'
+alias gc='git commit'
+alias gcb='git-current-branch'
+# outgoing commits
+alias gout='git-unpushed'
+# incoming commits
+alias ginc='git-unpulled'
+
